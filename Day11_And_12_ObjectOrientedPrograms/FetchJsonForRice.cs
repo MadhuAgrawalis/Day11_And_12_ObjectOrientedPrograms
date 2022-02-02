60c0474276031dd5adbe2f538f0d1b021ab7f33e
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-02-02 15:58:59 +0530

    Uc1

---
+++ Day11_And_12_ObjectOrientedPrograms/FetchJsonForRice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace InventoryManagement
+{
+    public class FetchJsonForRice
+    {
+        public object JsonConvert { get; private set; }
+
+        public Rice ReadTheJsonFile(string Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+
+                }
+            }
+        }
+    }
+}
